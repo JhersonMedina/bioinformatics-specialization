b5df576dde26e7d40f2ee6a35a662977e3b09796
JhersonMedina <jherson.medina@instaleap.io>
2023-05-10 18:23:21 -0500

    II debruijn kmer

---
+++ bioinformatics-ii/week1/debruijnkmer.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+#include <iomanip>
+
+using namespace std;
+/* *
+ *
+ * Too many mind, no mind.
+ *
+ * */
+void debruijn(string s, int k, map <string, vector <string>>& graph) {
+	for (int i = 0; i + k - 1 < s.size(); i++) {
+		string from = s.substr(i, k - 1);
+		string to = s.substr(i + 1, k - 1);
+		graph[from].push_back(to);
+	}
+}
+void debruijn(vector <string>& patterns) {
+	int k = patterns[0].size();
+	map <string, vector <string>> graph;
+	for (auto& s : patterns) debruijn(s, k, graph);
+
+	for (auto& u: graph) {
+		cout << u.first << ": ";
+		sort(u.second.begin(), u.second.end());
+		for (int i = 0; i < u.second.size(); i++) {
+			if (i) cout << ' ';
+			cout << u.second[i];
+		}
+		cout << '\n';
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	string s; 
+	vector <string> patterns;
+	while (cin >> s) patterns.push_back(s);
+	debruijn(patterns);
+	return 0;
+}
